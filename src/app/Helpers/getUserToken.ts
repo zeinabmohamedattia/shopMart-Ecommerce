f60cfaeb3c8459c9d5dc2c1e7e417c2f58fb6610
zeinabmohamedattia <zenabmohamed858@gmail.com>
2025-12-31 01:06:02 +0200

    tokenupdate

--- src/app/Helpers/getUserToken.ts
+++ src/app/Helpers/getUserToken.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers"
 
 export async function getUserToken() {
     const myCookies = await cookies()
-    const decodedToken = myCookies.get("next-auth.session-token")?.value || myCookies.get("__Secure-next-auth.session-token")?.value
+    const decodedToken = myCookies.get("next-auth.session-token")?.value || myCookies.get("__secure-next-auth.session-token")?.value
     const accessToken = await decode({ token: decodedToken, secret: process.env.NEXTAUTH_SECRET! })
     return accessToken?.token
 }
